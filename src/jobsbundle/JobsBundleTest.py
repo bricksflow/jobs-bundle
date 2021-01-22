800b3c7937bf1bb9ce8bc38a4614125c883896b4
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-22 08:26:26 +0100

    Updated for Pyfony 0.7.*

--- src/jobsbundle/JobsBundleTest.py
+++ src/jobsbundle/JobsBundleTest.py
@@ -1,14 +1,12 @@
 import unittest
 from injecta.testing.servicesTester import testServices
-from jobsbundle.containerInit import initContainer
+from pyfonycore.bootstrap import bootstrappedContainer
 
 class JobsBundleTest(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.__container = initContainer('test')
-
     def test_init(self):
-        testServices(self.__container)
+        container = bootstrappedContainer.init('test')
+        testServices(container)
 
 if __name__ == '__main__':
     unittest.main()
