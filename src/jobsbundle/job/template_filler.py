0619a503557eca0336d0213eec750f62c8060a12
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-03-28 17:17:46 +0200

    Fix {{placeholder}} problems

--- src/jobsbundle/job/template_filler.py
+++ src/jobsbundle/job/template_filler.py
@@ -3,11 +3,21 @@ import re
 
 def fill_template(template: str, replacements: dict):
     def convert(m):
-        placeholder = m.group(1)
+        placeholder = m.group(0)[1:-1]
 
         if placeholder not in replacements:
             raise Exception("Value for placeholder {" + placeholder + "} not defined")
 
         return str(replacements[placeholder])
 
-    return re.sub(r"\{([^\}]+)\}", convert, template)
+    placeholders = set(re.findall(r"{(\w+)}", template))
+    double_placeholders = set(re.findall(r"{{(\w+)}}", template))
+
+    placeholders -= double_placeholders
+
+    if not placeholders:
+        return template
+
+    pattern = re.compile("|".join(map(lambda x: "{" + x + "}", placeholders)))
+
+    return pattern.sub(convert, template)
